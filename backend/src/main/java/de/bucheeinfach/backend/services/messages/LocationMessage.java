95e79daa716af20174916cb68b2a450b54e7de13
Ugur Kartal <kartalugur@gmail.com>
2024-05-05 21:50:06 +0200

    add location update operation

--- backend/src/main/java/de/bucheeinfach/backend/services/messages/LocationMessage.java
+++ backend/src/main/java/de/bucheeinfach/backend/services/messages/LocationMessage.java
@@ -3,4 +3,5 @@ package de.bucheeinfach.backend.services.messages;
 public class LocationMessage {
     private LocationMessage(){}
     public static final String LOCATION_NOT_FOUND = "Standort nicht gefunden";
+    public static final String LOCATION_NAME_ALREADY_EXISTS = "Standortname existiert bereits";
 }
